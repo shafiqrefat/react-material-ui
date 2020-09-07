4240efe57f931fbb7cff9d38b739309678c4607e
shafiqrefat <shafiqrefat@gmail.com>
2020-09-07 23:22:08 +0600

    material-ui with API calling project done

--- src/Comment/Comment.js
+++ src/Comment/Comment.js
@@ -1,7 +1,8 @@
 import React from 'react';
+import {ListItem, Avatar, ListItemAvatar, ListItemText, Typography} from "@material-ui/core";
 
 const Comment = (props) => {
-    const {comment} = props;
+    const {name,email,body} = props.comment;
     const commentStyle = {
         border: '1px solid gray',
         borderRadius: '15px',
@@ -11,11 +12,26 @@ const Comment = (props) => {
         backgroundColor: 'lightcyan'
     }
     return (
-        <div style={commentStyle}>
-            <h3>Name : {comment.name}</h3>
-            <h4>Email : {comment.email}</h4>
-            <h5>Body : {comment.body}</h5>
-        </div>
+        <>
+        <ListItem alignItems="flex-start">
+            <ListItemAvatar>
+                <Avatar src={`https://randomuser.me/api/portraits/men/75.jpg`} alt="" />
+            </ListItemAvatar>
+            <ListItemText style={commentStyle}
+                primary= {name}
+                secondary={
+                    <>
+                        <Typography component="span" variant="body2" color="textPrimary">
+                            {email}
+                        </Typography> <br/>
+                        <Typography component="span" variant="body2" color="textPrimary">
+                            {body}
+                        </Typography>
+                    </>
+                }
+            ></ListItemText>
+        </ListItem>
+    </>
     );
 };
 
